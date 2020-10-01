44ad86a928e3591d7336e1dcf3d3921a00811ad0
Hitesh Patel <hbp976@gmail.com>
2020-10-01 11:03:03 -0600

    footer is created

---
+++ templates/footer.php
@@ -0,0 +1,10 @@
+    <footer>
+        <h2>Footer Details</h2>
+        <p>
+            &copy;
+            <?php echo date( 'Y' ); ?>
+            TECHCareers
+        </p>
+    </footer>
+</body>
+</html>
---
+++ templates/header.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title></title>
+
+    <link rel="stylesheet" type="text/css" href="css/main.css">
+
+    <script type="text/JavaScript" src="js/scripts.js" defer></script>
+
+</head>
+<body>
+    <h1>WebPage</h1>
+
+    
